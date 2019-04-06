72299366432530ace0d460f8688ab3428aeb09e5
EmilyHao <49345388+EmilyHao@users.noreply.github.com>
2019-04-06 15:08:21 -0400

    buttonss

--- hacktj/App.js
+++ hacktj/App.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
+	  _onPressButton() {
+    Alert.alert('You tapped the button!')
+    }
       <View style={styles.container}>
 		  <Text style={styles.titletext}>SafeWalk - Get Home Safe</Text>
+		  
       </View>
-    );
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+          />
+      </View>
+	);
   }
 }
 var width = Dimensions.get('window').width;
 var height = Dimensions.get('window').height;
 const styles = StyleSheet.create({
-
+   container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#194d33',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -30,4 +44,6 @@ const styles = StyleSheet.create({
 	marginTop: 0.2 * height,
 	marginBottom: 0.6 * height,
   }
+  
+  
 });
